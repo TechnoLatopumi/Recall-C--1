0528a31b2e027fce1b007317031da2759c94fd19
TechnoLatopumi <tegartpp@gmail.com>
2022-08-03 17:19:00 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string characterName = "Tom";
+        int characterAge ;
+        characterAge = 20;
+        Console.WriteLine("Hello my name is "+characterName);
+        Console.WriteLine("My Age is "+ characterAge);
+        characterName = "Jim" ;
+        Console.WriteLine("And his name is " + characterName) ;
+        
+
+        Console.ReadLine();
+    }
+}
